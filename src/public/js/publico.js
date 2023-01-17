42917cacbabcedf207e22bfdbf74c7222d5da615
Jaindris Sosa <jaindrissosajsd@gmail.com>
2023-01-16 21:24:24 -0500

    Agregar Audio.mp3

--- src/public/js/publico.js
+++ src/public/js/publico.js
@@ -15,6 +15,9 @@ const socket = io();
 
 socket.on("estado-actual", (payload) => {
   const [ticket1, ticket2, ticket3, ticket4] = payload;
+  const audio = new Audio("./audio/new-ticket.mp3");
+  audio.play();
+
   if (ticket1) {
     lblTicket1.innerText = `Ticket ${ticket1.number}`;
     lblEscritorio1.innerText = ticket1.desk;
